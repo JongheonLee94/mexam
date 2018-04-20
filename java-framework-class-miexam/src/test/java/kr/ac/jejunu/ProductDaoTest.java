169fa119616dea18c55cb447fa88326731ae4ae2
Jongheon94 <ismir1449@gmail.com>
2018-04-20 15:42:35 +0900

    2번
    제주 한라 두 회사 납품

--- java-framework-class-miexam/src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ java-framework-class-miexam/src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -10,9 +10,11 @@ import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
     ProductDao productDao;
+    ProductDao halaProductDao;
     @Before
     public void setup() {
-        productDao = new ProductDao();
+        productDao = new JejuProductDao();
+        halaProductDao= new HalaProductDao();
     }
 
     @Test
@@ -35,6 +37,30 @@ public class ProductDaoTest {
         Long id = productDao.insert(product);
 
         Product insertedproduct = productDao.get(id);
+        assertEquals(id, insertedproduct.getId());
+        assertEquals(product.getTitle(), insertedproduct.getTitle());
+        assertEquals(product.getPrice(), insertedproduct.getPrice());
+    }
+    @Test
+    public void halaget() throws SQLException, ClassNotFoundException {
+
+        Long id = 1L;
+        String title = "제주감귤";
+        Integer price = 15000;
+
+        Product product = halaProductDao.get(id);
+        assertEquals(id, product.getId());
+        assertEquals(title, product.getTitle());
+        assertEquals(price, product.getPrice());
+    }
+    @Test
+    public void halaadd() throws SQLException, ClassNotFoundException {
+        Product product=new Product();
+        product.setTitle( "빵" );
+        product.setPrice( 10000 );
+        Long id = halaProductDao.insert(product);
+
+        Product insertedproduct = halaProductDao.get(id);
         assertEquals(id, insertedproduct.getId());
         assertEquals(product.getTitle(), insertedproduct.getTitle());
         assertEquals(product.getPrice(), insertedproduct.getPrice());
