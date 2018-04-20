8d1f29cf9bdd92ea2a602a7f4957cb5547a96178
Jongheon94 <ismir1449@gmail.com>
2018-04-20 15:37:22 +0900

    1번
    추가기능

--- java-framework-class-miexam/src/test/java/kr/ac/jejunu/ProductDaoTest.java
+++ java-framework-class-miexam/src/test/java/kr/ac/jejunu/ProductDaoTest.java
@@ -1,18 +1,23 @@
 package kr.ac.jejunu;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.annotation.Bean;
 
 import java.sql.SQLException;
 
 import static org.junit.Assert.assertEquals;
 
 public class ProductDaoTest {
+    ProductDao productDao;
+    @Before
     public void setup() {
+        productDao = new ProductDao();
     }
 
     @Test
     public void get() throws SQLException, ClassNotFoundException {
-        ProductDao productDao = new ProductDao();
+
         Long id = 1L;
         String title = "제주감귤";
         Integer price = 15000;
@@ -21,5 +26,17 @@ public class ProductDaoTest {
         assertEquals(id, product.getId());
         assertEquals(title, product.getTitle());
         assertEquals(price, product.getPrice());
+    }
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        Product product=new Product();
+        product.setTitle( "오메기떡" );
+        product.setPrice( 10000 );
+        Long id = productDao.insert(product);
+
+        Product insertedproduct = productDao.get(id);
+        assertEquals(id, insertedproduct.getId());
+        assertEquals(product.getTitle(), insertedproduct.getTitle());
+        assertEquals(product.getPrice(), insertedproduct.getPrice());
     }
 }
